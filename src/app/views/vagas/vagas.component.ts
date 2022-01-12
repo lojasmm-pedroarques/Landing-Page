c7e9417db95407579dc38eb784ed55f8ed4a772d
pedro.arques <pedro.arques@lojasmm.com>
2022-01-12 10:38:17 -0300

    criação das divisórias

--- src/app/views/vagas/vagas.component.ts
+++ src/app/views/vagas/vagas.component.ts
@@ -10,8 +10,29 @@ export class VagasComponent implements OnInit {
 
   
   constructor(private renderer: Renderer2, private el: ElementRef) {
+    var set = new Set()
+    for(let i = 0; i < vagas.length; i++) {
+      set.add(vagas[i].categoria)
+     }
+
+    //declara a lista e atribuí os valores que estão no set
+    var lista: any
+    lista = Array.from(set)
+    
+    //cria a div que irá conter os cards
 
     const div = this.renderer.createElement('div');
+    this.renderer.addClass(div, 'cards__vagas')
+
+    for(let i = 0; i < lista.length; i++) { 
+      const matToolbar = this.renderer.createElement('mat-toolbar');
+      const categoria = this.renderer.createText(lista[i]);
+      this.renderer.appendChild(matToolbar, categoria);
+      this.renderer.addClass(matToolbar, 'mat-toolbar')
+      this.renderer.addClass(matToolbar, 'header')
+      this.renderer.addClass(matToolbar, 'mat-toolbar-single-row')
+      this.renderer.appendChild(this.el.nativeElement, matToolbar);
+    }
     
     for(let i = 0; i < vagas.length; i++) {
       const matCard = this.renderer.createElement('mat-card');
@@ -30,9 +51,11 @@ export class VagasComponent implements OnInit {
       
       this.renderer.appendChild(matCard, title);
       this.renderer.appendChild(matCard, matCardContent);
+
       this.renderer.appendChild(div, matCard);
 
     }
+
     this.renderer.appendChild(this.el.nativeElement, div);
 
    }
