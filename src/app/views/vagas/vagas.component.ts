7c960b083a79c7038b3d88f1bd3a2c89225600c9
pedro.arques <pedro.arques@lojasmm.com>
2022-01-13 09:48:05 -0300

    adicionadas imagens aos cards

--- src/app/views/vagas/vagas.component.ts
+++ src/app/views/vagas/vagas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { MatToolbar } from '@angular/material/toolbar';
 import vagas from '../../../../output.json'
 
 @Component({
@@ -10,47 +12,61 @@ export class VagasComponent implements OnInit {
 
   
   constructor(private renderer: Renderer2, private el: ElementRef) {
+    var image_list = new Array('../../../assets/images/images-cards/1.jpg', '../../../assets/images/images-cards/2.jpg', 
+    '../../../assets/images/images-cards/3.jpg', '../../../assets/images/images-cards/4.jpg', '../../../assets/images/images-cards/5.jpg',
+    '../../../assets/images/images-cards/6.webp', '../../../assets/images/images-cards/7.jpg', '../../../assets/images/images-cards/8.jpg', 
+    '../../../assets/images/images-cards/9.jpg', '../../../assets/images/images-cards/10.jpg', '../../../assets/images/images-cards/11.jpg',
+    '../../../assets/images/images-cards/12.webp', '../../../assets/images/images-cards/13.jpg', '../../../assets/images/images-cards/14.webp',
+    '../../../assets/images/images-cards/15.webp', '../../../assets/images/images-cards/16.jpg', '../../../assets/images/images-cards/17.webp',)
+
     var set = new Set()
     for(let i = 0; i < vagas.length; i++) {
-      set.add(vagas[i].categoria)
+      set.add(vagas[i].categoria);
      }
 
     //declara a lista e atribuí os valores que estão no set
-    var lista: any
-    lista = Array.from(set)
+    var list: any;
+    list = Array.from(set);
     
     //cria a div que irá conter os cards
 
     const div = this.renderer.createElement('div');
-    this.renderer.addClass(div, 'cards__vagas')
-
-    for(let i = 0; i < lista.length; i++) { 
-      const matToolbar = this.renderer.createElement('mat-toolbar');
-      const categoria = this.renderer.createText(lista[i]);
-      this.renderer.appendChild(matToolbar, categoria);
-      this.renderer.addClass(matToolbar, 'mat-toolbar')
-      this.renderer.addClass(matToolbar, 'header')
-      this.renderer.addClass(matToolbar, 'mat-toolbar-single-row')
-      this.renderer.appendChild(this.el.nativeElement, matToolbar);
-    }
+    this.renderer.addClass(div, 'cards__vagas');
     
     for(let i = 0; i < vagas.length; i++) {
-      const matCard = this.renderer.createElement('mat-card');
-      this.renderer.addClass(matCard, 'mat-card')
-      this.renderer.addClass(matCard, 'cards__card')
 
+      var randomNum = Math.floor(Math.random() * image_list.length);
+
+      const matCard = this.renderer.createElement('mat-card');
+      this.renderer.addClass(matCard, 'mat-card');
+      this.renderer.addClass(matCard, 'cards__card');
 
       const title = this.renderer.createElement('mat-card-title');
       const titleText = this.renderer.createText(vagas[i].titulo);
       this.renderer.appendChild(title, titleText);
-      this.renderer.addClass(matCard, 'mat-card-title')
+
+      const matCardImage = this.renderer.createElement('img')
+      this.renderer.setAttribute(matCardImage, "src", image_list[randomNum]);
+      this.renderer.addClass(matCardImage, 'cards__image');
 
       const matCardContent = this.renderer.createElement('mat-card-content');
-      const matCardContentText = this.renderer.createText(vagas[i].descricao)
-      this.renderer.appendChild(matCardContent, matCardContentText)
+      const matCardContentText = this.renderer.createText(vagas[i].descricao);
+      this.renderer.appendChild(matCardContent, matCardImage);
+      this.renderer.appendChild(matCardContent, matCardContentText);
       
+      const matCardActions = this.renderer.createElement('mat-card-actions');
+      const matCardActionsButton = this.renderer.createElement('button');
+      const buttonText = this.renderer.createText('Saiba Mais');
+      
+      this.renderer.addClass(matCardActionsButton, 'mat-button');
+      this.renderer.addClass(matCardActionsButton, 'button-saibamais');
+      
+      this.renderer.appendChild(matCardActionsButton, buttonText);
+      this.renderer.appendChild(matCardActions, matCardActionsButton);
+
       this.renderer.appendChild(matCard, title);
       this.renderer.appendChild(matCard, matCardContent);
+      this.renderer.appendChild(matCard, matCardActions);
 
       this.renderer.appendChild(div, matCard);
 
